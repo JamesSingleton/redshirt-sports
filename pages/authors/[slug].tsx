33f917d3c348bf232ca6ccedcbfd77a30ec1710d
James Singleton <jamesrsingleton1@gmail.com>
2021-11-11 23:15:43 -0700

    feat: authors page

--- pages/authors/[slug].tsx
+++ pages/authors/[slug].tsx
@@ -1,24 +1,205 @@
+import { Fragment } from 'react'
 import { GetStaticProps, GetStaticPaths } from 'next'
+import Image from 'next/image'
 import { NextSeo } from 'next-seo'
+import cn from 'classnames'
+import { Tab } from '@headlessui/react'
 import { Layout } from '@components/common'
 import { getClient, sanityClient } from '@lib/sanity.server'
 import { authorBySlugQuery, authorSlugsQuery } from '@lib/sanityGroqQueries'
+import { urlForImage, PortableText } from '@lib/sanity'
+
+import styles from './Authors.module.css'
 
 interface AuthorProps {
   author: {
     name: string
     slug: string
     image: string
+    backgroundImage: string
     bio: string
   }
 }
+
+const tabs = [
+  { name: 'About', href: '#', current: true },
+  { name: 'Social', href: '#', current: false },
+]
+
+const profile = {
+  name: 'Ricardo Cooper',
+  imageUrl:
+    'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+  coverImageUrl:
+    'https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+  about: `
+    <p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+    <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>
+  `,
+  fields: {
+    Phone: '(555) 123-4567',
+    Email: 'ricardocooper@example.com',
+    Title: 'Senior Front-End Developer',
+    Team: 'Product Development',
+    Location: 'San Francisco',
+    Sits: 'Oasis, 4th floor',
+    Salary: '$145,000',
+    Birthday: 'June 8, 1990',
+  },
+}
+
+const team = [
+  {
+    name: 'Leslie Alexander',
+    handle: 'lesliealexander',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Michael Foster',
+    handle: 'michaelfoster',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Dries Vincent',
+    handle: 'driesvincent',
+    role: 'Manager, Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    name: 'Lindsay Walton',
+    handle: 'lindsaywalton',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+]
 
 const Author = ({ author }: AuthorProps) => {
   return (
     <>
       <NextSeo title={`${author.name} Profile`} />
-      <div>
-        <h1>{author.name}</h1>
+      <div className="max-w-7xl mx-auto">
+        <div>
+          <div className="relative h-32 w-full lg:h-48">
+            <Image
+              src={
+                urlForImage(author.backgroundImage)
+                  .height(196)
+                  .width(1020)
+                  .url()!
+              }
+              alt=""
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+              <div className="flex">
+                <div className={styles.imageWrapper}>
+                  <Image
+                    className="rounded-full ring-4 ring-white"
+                    src={
+                      urlForImage(author.image).height(128).width(128).url()!
+                    }
+                    alt=""
+                    height="128"
+                    width="128"
+                  />
+                </div>
+              </div>
+              <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                <div className="sm:hidden 2xl:block mt-6 min-w-0 flex-1">
+                  <h1 className="text-2xl font-bold text-gray-900 truncate">
+                    {author.name}
+                  </h1>
+                </div>
+              </div>
+            </div>
+            <div className="hidden sm:block 2xl:hidden mt-6 min-w-0 flex-1">
+              <h1 className="text-2xl font-bold text-gray-900 truncate">
+                {author.name}
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        {/* Tabs */}
+        <div className="mt-6 sm:mt-2 2xl:mt-5">
+          <div>
+            <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+              <Tab.Group>
+                <Tab.List
+                  className="border-b border-gray-200 -mb-px flex space-x-8"
+                  aria-label="Tabs"
+                >
+                  {tabs.map((tab) => (
+                    <Tab as={Fragment} key={tab.name}>
+                      {({ selected }) => (
+                        <button
+                          className={cn(
+                            selected
+                              ? 'border-pink-500 text-gray-900'
+                              : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                            'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
+                          )}
+                        >
+                          {tab.name}
+                        </button>
+                      )}
+                    </Tab>
+                  ))}
+                </Tab.List>
+                <Tab.Panels>
+                  <Tab.Panel>
+                    <div className="mt-6">
+                      <div className="mt-1 text-sm text-gray-900 space-y-5">
+                        <PortableText blocks={author.bio} />
+                      </div>
+                    </div>
+                  </Tab.Panel>
+                </Tab.Panels>
+              </Tab.Group>
+            </div>
+          </div>
+        </div>
+
+        {/* Team member list */}
+        <div className="mt-8 max-w-5xl mx-auto px-4 pb-12 sm:px-6 lg:px-8">
+          <h2 className="text-sm font-medium text-gray-500">Team members</h2>
+          <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+            {team.map((person) => (
+              <div
+                key={person.handle}
+                className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500"
+              >
+                <div className="flex-shrink-0">
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={person.imageUrl}
+                    alt=""
+                  />
+                </div>
+                <div className="flex-1 min-w-0">
+                  <a href="#" className="focus:outline-none">
+                    <span className="absolute inset-0" aria-hidden="true" />
+                    <p className="text-sm font-medium text-gray-900">
+                      {person.name}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate">
+                      {person.role}
+                    </p>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )
@@ -31,10 +212,10 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     slug: params?.slug,
   })
 
-  console.log({ author })
   if (!author) {
     return { notFound: true }
   }
+
   return {
     props: {
       author,
