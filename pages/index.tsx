dd0468e12c4cdfc4886171ddf906b1ea500466f5
James Singleton <jamesrsingleton1@gmail.com>
2021-12-07 10:03:33 -0700

    feat(home): add podcast section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next'
+import Image from 'next/image'
 import { NextSeo } from 'next-seo'
 import { Layout } from '@components/common'
 import { Hero, FeaturedArticleSection, ArticlesSection } from '@components/home'
@@ -40,6 +41,51 @@ function Home({ heroPost, morePosts, featuredArticles }: HomeProps) {
         <aside className="px-4 py-4 sm:px-0 lg:py-0 lg:col-span-3">
           <div className="sticky top-28 space-y-4">
             {morePosts.length > 0 && <ArticlesSection posts={morePosts} />}
+            <div className="bg-white rounded-lg shadow">
+              <h2 className="h-11 mx-3 flex items-center justify-between text-base font-medium text-gray-900 border-b border-gray-300">
+                Podcasts
+              </h2>
+              <div className="mt-6 flex flex-col overflow-hidden rounded-b-lg">
+                <div className="flex-shrink-0 text-center">
+                  <Image
+                    src="/images/FCS_Nation_Logo.jpeg"
+                    alt="FCS Nation Logo"
+                    width="176"
+                    height="176"
+                  />
+                </div>
+                <div className="flex-1 bg-white p-3 flex flex-col justify-between items-center">
+                  <div className="flex-1">
+                    <a
+                      href="https://podcasts.apple.com/us/podcast/fcs-nation/id1436799349?mt=2&ls=1"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image
+                        src="/images/US_UK_Apple_Podcasts_Listen_Badge_RGB.svg"
+                        height="38"
+                        width="185"
+                        alt="Listen on Apple Podcasts"
+                        quality="65"
+                      />
+                    </a>
+                    <a
+                      href="https://www.stitcher.com/show/fcs-nation"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image
+                        src="/images/stitcher_logo.svg"
+                        height="38"
+                        width="185"
+                        alt="Listen on Stitcher"
+                        quality="65"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
             {featuredArticles.length > 0 && <FeaturedArticleSection />}
           </div>
         </aside>
