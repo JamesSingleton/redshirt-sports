a03a89aacef59157a7b7d317b1fedd2af35e6510
James Singleton <jamesrsingleton1@gmail.com>
2021-12-10 19:18:16 -0700

    chore: remove ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,15 +2,9 @@ import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   render() {
-    const ADSENSE_ID = process.env.NEXT_PUBLIC_ADSENSE_ID
     return (
       <Html lang="en-US" className="h-full bg-gray-100">
-        <Head>
-          <script
-            async
-            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?${ADSENSE_ID}`}
-          />
-        </Head>
+        <Head />
         <body className="loading">
           <Main />
           <NextScript />
