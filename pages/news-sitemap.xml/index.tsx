f9569a8c020d395d66fc23798a1065e8e279696c
James Singleton <jamesrsingleton1@gmail.com>
2021-12-21 11:42:40 -0500

    feat: add google news sitemap

---
+++ pages/news-sitemap.xml/index.tsx
@@ -0,0 +1,41 @@
+import { GetServerSideProps } from 'next'
+import { sanityClient } from '@lib/sanity.server'
+import { allPosts } from '@lib/sanityGroqQueries'
+import type { Post } from '@lib/types/post'
+
+const NewsSitemap = () => {}
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const baseUrl = 'https://www.redshirtsports.xyz'
+  const posts = await sanityClient.fetch(allPosts)
+
+  const postsSitemap = posts.map(
+    (post: Post) => `
+      <loc>${baseUrl}/${post.slug}</loc>
+      <news:news>
+        <news:publication>
+          <news:name>Redshirt Sports</news:name>
+          <news:language>en</news:language>
+        </news:publication>
+        <news:publication_date>${post.publishedAt}</news:publication_date>
+        <news:title>${post.title}</news:title>
+      </news:news>
+    `
+  )
+
+  const createSitemap = () => `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9">
+      ${postsSitemap.map((post: any) => `<url>${post}</url>`).join('')}
+  </urlset>
+  `
+
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(createSitemap())
+  res.end()
+
+  return {
+    props: {},
+  }
+}
+
+export default NewsSitemap
