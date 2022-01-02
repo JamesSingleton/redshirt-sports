6ac906fe7cbe01fb1d3ca085621ac529cc335018
James Singleton <jamesrsingleton1@gmail.com>
2022-01-01 17:19:13 -0700

    fix: add missing alt text

--- pages/advertising.tsx
+++ pages/advertising.tsx
@@ -82,7 +82,7 @@ const Advertising = ({ advertising }: AdvertisingProps) => {
                 <Image
                   className="absolute inset-0 h-full w-full object-cover"
                   src="https://images.unsplash.com/photo-1513757378314-e46255f6ed16?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
-                  alt=""
+                  alt="Blank billboard"
                   layout="fill"
                   objectFit="cover"
                 />
