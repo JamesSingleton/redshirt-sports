7e31e8e2d3ab7446395334022fc4d8e155f6398a
James Singleton <jamesrsingleton1@gmail.com>
2022-01-07 15:26:16 -0700

    chore: remove prefetch

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,8 @@
 import React, { FC } from 'react'
-import { useRouter } from 'next/router'
-import { m, LazyMotion, AnimatePresence, domAnimation } from 'framer-motion'
 import { LogoJsonLd } from 'next-seo'
 import { Navbar, Footer } from '@components/common'
 
 const Layout: FC = ({ children }) => {
-  const router = useRouter()
   return (
     <>
       <LogoJsonLd
@@ -14,20 +11,7 @@ const Layout: FC = ({ children }) => {
       />
       <div className="h-full bg-gray-50 mx-auto transition-colors duration-150 relative overflow-hidden">
         <Navbar />
-        <LazyMotion features={domAnimation}>
-          <AnimatePresence exitBeforeEnter>
-            <m.main
-              key={router.route}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.7 }}
-              className="fit"
-            >
-              {children}
-            </m.main>
-          </AnimatePresence>
-        </LazyMotion>
+        <main className="fit">{children}</main>
         <Footer />
       </div>
     </>
