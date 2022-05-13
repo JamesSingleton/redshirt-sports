8dafbdb47b7b98221011ed4ab2d9b9fa26adefe5
James Singleton <jamesrsingleton1@gmail.com>
2022-05-13 15:51:15 -0700

    chore: remove semicolon

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const securityHeaders = [
   },
   {
     key: 'Strict-Transport-Security',
-    value: 'max-age=63072000; includeSubDomains; preload;',
+    value: 'max-age=63072000; includeSubDomains; preload',
   },
 ]
 
