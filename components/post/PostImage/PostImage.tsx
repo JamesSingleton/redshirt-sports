e42bd316ce0dce47cf462f13d15454d278dd5958
James Singleton <jamesrsingleton1@gmail.com>
2022-05-16 21:58:14 -0700

    chore: add objectFit cover to post image

--- components/post/PostImage/PostImage.tsx
+++ components/post/PostImage/PostImage.tsx
@@ -14,13 +14,14 @@ const PostImage: FC<PostImageProps> = ({ image }) => (
   <div className="container my-10 mx-auto px-4 sm:my-12 xl:px-32">
     <figure>
       <Image
-        className="h-full w-full rounded-md object-cover"
+        className="h-full w-full rounded-md"
         src={urlForImage(image).width(1280).height(606).url()!}
         width={1280}
         height={606}
         alt={image.caption}
         layout="responsive"
         sizes="50vw"
+        objectFit="cover"
         priority
       />
       <figcaption className="mt-3 flex text-sm">
