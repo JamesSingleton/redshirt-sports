57a4529795589f81e83b0fae8951be756e333e6f
James Singleton <jamesrsingleton1@gmail.com>
2022-08-02 08:31:17 -0700

    chore: show no results info

--- pages/search.tsx
+++ pages/search.tsx
@@ -60,9 +60,31 @@ const Search = ({ posts }: SearchProps) => {
         <section className="mx-auto max-w-7xl py-12 md:py-16 lg:py-20 lg:px-8">
           <div className="w-full lg:grid lg:grid-cols-3 lg:gap-8 xl:gap-12">
             <div className="col-span-2">
-              {posts.map((post, key) => (
-                <HorizontalCard post={post} key={post._id} />
-              ))}
+              {posts.length > 0 &&
+                posts.map((post, key) => <HorizontalCard post={post} key={post._id} />)}
+              {!posts.length && (
+                <div className="text-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="mx-auto h-20 w-20 text-slate-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      vectorEffect="non-scaling-stroke"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"
+                    />
+                  </svg>
+                  <h3 className="mt-2 font-cal text-xl font-medium text-slate-900">No Results</h3>
+                  <p className="mt-1 text-base text-slate-500">
+                    {`We couldn't find a match for "${query}".`}
+                  </p>
+                </div>
+              )}
             </div>
             <div className="mt-12 w-full sm:mt-16 lg:col-span-1 lg:mt-0">
               <SocialMediaFollow />
@@ -78,12 +100,6 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const posts = await sanityClient.fetch(searchQuery, {
     searchTerm: context.query.query,
   })
-
-  if (!posts.length) {
-    return {
-      notFound: true,
-    }
-  }
 
   return {
     props: {
