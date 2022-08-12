daa9b2ff2a014883a7c4aef89469d2400c2d99e1
James Singleton <jamesrsingleton1@gmail.com>
2022-08-12 16:18:04 -0700

    chore: update revalidate api

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,18 +1,34 @@
-import { isValidRequest } from '@sanity/webhook'
+import { isValidRequest, SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
 import { withSentry } from '@sentry/nextjs'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-const secret = process.env.SANITY_WEBHOOK_SECRET
+const secret = process.env.SANITY_WEBHOOK_SECRET as string
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
+async function readBody(readable: any) {
+  const chunks = []
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+  }
+  return Buffer.concat(chunks).toString('utf8')
+}
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string
+  const body = await readBody(req)
   if (req.method !== 'POST') {
     console.error('Only POST requests are accepted')
-    return res.status(401).json({ message: 'Only POST requests are accepted' })
+    return res.status(401).json({ success: false, message: 'Only POST requests are accepted' })
   }
 
-  if (!isValidRequest(req, secret!)) {
-    res.status(401).json({ message: 'Invalid signature' })
+  if (!isValidSignature(body, signature, secret)) {
+    res.status(401).json({ success: false, message: 'Invalid signature' })
     return
   }
 
@@ -26,12 +42,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         await res.revalidate(`/${slug}`)
         await res.revalidate('/')
         await res.revalidate(`/authors/${authorSlug}`)
-        return res.json({ message: `Revalidated "${type}" with slug "${slug}"` })
+        return res.json({ success: true, message: `Revalidated "${type}" with slug "${slug}"` })
     }
 
     return res.json({ message: 'No managed type' })
   } catch (err) {
-    return res.status(500).send({ message: 'Error revalidating' })
+    return res.status(500).send({ success: false, message: 'Error revalidating' })
   }
 }
 
