7e41c62fa1337dbf44dc0ced555d152e8f6ac0cc
James Singleton <jamesrsingleton1@gmail.com>
2022-08-18 08:37:08 -0700

    chore: move sanity to tsx

--- lib/config.ts
+++ lib/config.ts
@@ -1,11 +1,6 @@
 export const sanityConfig = {
-  // Find your project ID and dataset in `sanity.json` in your studio project
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: false,
-  // useCdn == true gives fast, cheap responses using a globally distributed cache.
-  // Set this to false if your application require the freshest possible
-  // data always (potentially slightly slower and a bit more expensive).
   apiVersion: '2021-10-21',
-  // see https://www.sanity.io/docs/api-versioning for how versioning works
 }
--- lib/sanity.tsx
+++ lib/sanity.tsx
@@ -2,15 +2,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import createImageUrlBuilder from '@sanity/image-url'
-import { PortableText } from '@portabletext/react'
+import { PortableText, PortableTextMarkComponentProps } from '@portabletext/react'
 import { getImageDimensions } from '@sanity/asset-utils'
 import { sanityConfig } from '@lib/config'
 import { Tweet } from '@components/ui'
 
-export const imageBuilder = createImageUrlBuilder(sanityConfig)
-export const urlForImage = (source) => imageBuilder.image(source).auto('format').fit('min')
+export const imageBuilder = createImageUrlBuilder(sanityConfig as any)
+export const urlForImage = (source: any) => imageBuilder.image(source).auto('format').fit('min')
 
-const ImageComponent = ({ value, isInline }) => {
+const ImageComponent = ({ value, isInline }: { value: any; isInline: any }) => {
   const { width, height } = getImageDimensions(value)
   return (
     <Image
@@ -24,13 +24,15 @@ const ImageComponent = ({ value, isInline }) => {
   )
 }
 
-const InternalLink = ({ children, value }) => (
-  <Link href={`/${value.slug.current}`} prefetch={false}>
-    <a>{children}</a>
-  </Link>
-)
+const InternalLink = ({ children, value }: PortableTextMarkComponentProps) => {
+  return (
+    <Link href={`/${value?.slug.current}`} prefetch={false}>
+      <a>{children}</a>
+    </Link>
+  )
+}
 
-const TwitterComponent = ({ value }) => {
+const TwitterComponent = ({ value }: { value: any }) => {
   return <Tweet id={value.id} metadata={JSON.stringify(value.metadata)} />
 }
 const myPortableTextComponents = {
@@ -43,6 +45,6 @@ const myPortableTextComponents = {
   },
 }
 
-export const PortableTextComponent = ({ value }) => (
-  <PortableText components={myPortableTextComponents} value={value} onMissingComponent={false} />
+export const PortableTextComponent = ({ value }: { value: any }) => (
+  <PortableText components={myPortableTextComponents} value={value} />
 )
