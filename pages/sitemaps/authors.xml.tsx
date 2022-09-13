68293209cfd45e78a9eb05f3ddcb1c3429db6b03
James Singleton <jamesrsingleton1@gmail.com>
2022-09-12 22:01:57 -0700

    feat: use next-sitemap (#44)

---
+++ pages/sitemaps/authors.xml.tsx
@@ -0,0 +1,23 @@
+import { GetServerSideProps } from 'next'
+import { getServerSideSitemap } from 'next-sitemap'
+
+import { sanityClient } from '@lib/sanity.server'
+import { getAllAuthorsForSitemap } from '@lib/queries'
+
+import type { Author } from '@types'
+
+export default function Sitemap() {}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const authors = await sanityClient.fetch<Author[]>(getAllAuthorsForSitemap)
+
+  const authorFields = authors.map((author) => ({
+    loc: `https://www.redshirtsports.xyz/authors/${author.slug}`,
+    lastmod: author._updatedAt,
+    priority: 0.5,
+  }))
+
+  const sitemap = [...authorFields]
+
+  return getServerSideSitemap(ctx, sitemap)
+}
