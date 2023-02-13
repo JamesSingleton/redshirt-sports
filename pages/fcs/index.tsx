6c2b4e94dbcd0c60c77c1a9ac0f958a9f7bd7d6e
James Singleton <jamesrsingleton1@gmail.com>
2023-02-13 08:05:48 -0700

    chore: use single layout component

--- pages/fcs/index.tsx
+++ pages/fcs/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
-import { Layout, SocialMediaFollow, SEO } from '@components/common'
+import { SocialMediaFollow, SEO } from '@components/common'
 import { ArticleList, CategoryHeader } from '@components/ui'
 import { sanityClient } from '@lib/sanity.server'
 import { fcsPostsQuery } from '@lib/queries'
@@ -92,28 +92,26 @@ const FCS = ({ posts, totalPosts, totalPages, currentPage }: fcsProps) => {
           url: 'https://www.redshirtsports.xyz/fcs',
         }}
       />
-      <Layout>
-        <CategoryHeader
-          title="Latest FCS Football News"
-          aboveTitle="Football Championship Subdivision"
-          breadCrumbPages={breadCrumbPages}
-        />
-        <section className="mx-auto max-w-xl px-4 py-12 sm:px-12 sm:py-16 md:max-w-3xl lg:max-w-7xl lg:px-8 lg:py-24">
-          <div className="w-full lg:grid lg:grid-cols-3 lg:gap-8 xl:gap-12">
-            <div className="col-span-2">
-              <ArticleList
-                articles={posts}
-                totalPages={totalPages}
-                totalPosts={totalPosts}
-                currentPage={currentPage}
-              />
-            </div>
-            <div className="mt-12 w-full sm:mt-16 lg:col-span-1 lg:mt-0">
-              <SocialMediaFollow />
-            </div>
+      <CategoryHeader
+        title="Latest FCS Football News"
+        aboveTitle="Football Championship Subdivision"
+        breadCrumbPages={breadCrumbPages}
+      />
+      <section className="mx-auto max-w-xl px-4 py-12 sm:px-12 sm:py-16 md:max-w-3xl lg:max-w-7xl lg:px-8 lg:py-24">
+        <div className="w-full lg:grid lg:grid-cols-3 lg:gap-8 xl:gap-12">
+          <div className="col-span-2">
+            <ArticleList
+              articles={posts}
+              totalPages={totalPages}
+              totalPosts={totalPosts}
+              currentPage={currentPage}
+            />
           </div>
-        </section>
-      </Layout>
+          <div className="mt-12 w-full sm:mt-16 lg:col-span-1 lg:mt-0">
+            <SocialMediaFollow />
+          </div>
+        </div>
+      </section>
     </>
   )
 }
--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next'
 
-import { Layout, SocialMediaFollow, SEO } from '@components/common'
+import { SocialMediaFollow, SEO } from '@components/common'
 import { sanityClient } from '@lib/sanity.server'
 import { getPrivacyPolicyPage } from '@lib/queries'
 import { PortableTextComponent } from '@lib/sanity'
@@ -30,28 +30,26 @@ const Privacy = ({ privacyPolicy }: privacyPolicyProps) => {
         }}
         canonical="https://www.redshirtsports.xyz/privacy"
       />
-      <Layout>
-        <PageHeader
-          heading="Privacy Policy"
-          subheading={
-            <>
-              Last updated on <Date dateString={privacyPolicy._updatedAt} />
-            </>
-          }
-        />
-        <section className="mx-auto max-w-7xl py-12 md:py-16 lg:py-20 lg:px-8">
-          <div className="w-full lg:flex">
-            <div className="lg:w-2/3">
-              <div className="prose prose-lg prose-indigo mx-auto px-5 sm:px-6 md:px-8 lg:mx-0 lg:px-0">
-                <PortableTextComponent value={privacyPolicy.body} />
-              </div>
-            </div>
-            <div className="mx-auto mt-12 w-full max-w-xl space-x-8 space-x-reverse px-4 sm:mt-16 sm:px-6 md:max-w-2xl md:px-8 lg:mt-0 lg:w-1/3 lg:max-w-none lg:px-0">
-              <SocialMediaFollow />
+      <PageHeader
+        heading="Privacy Policy"
+        subheading={
+          <>
+            Last updated on <Date dateString={privacyPolicy._updatedAt} />
+          </>
+        }
+      />
+      <section className="mx-auto max-w-7xl py-12 md:py-16 lg:py-20 lg:px-8">
+        <div className="w-full lg:flex">
+          <div className="lg:w-2/3">
+            <div className="prose prose-lg prose-indigo mx-auto px-5 sm:px-6 md:px-8 lg:mx-0 lg:px-0">
+              <PortableTextComponent value={privacyPolicy.body} />
             </div>
           </div>
-        </section>
-      </Layout>
+          <div className="mx-auto mt-12 w-full max-w-xl space-x-8 space-x-reverse px-4 sm:mt-16 sm:px-6 md:max-w-2xl md:px-8 lg:mt-0 lg:w-1/3 lg:max-w-none lg:px-0">
+            <SocialMediaFollow />
+          </div>
+        </div>
+      </section>
     </>
   )
 }
