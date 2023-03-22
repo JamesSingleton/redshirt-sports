b2a21aea9b7eeb420419acaa52f27470bfd29e75
James Singleton <jamesrsingleton1@gmail.com>
2023-03-22 10:04:15 -0700

    chore: increase max width of header

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
   return (
     <header className="bg-white">
       <nav
-        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        className="mx-auto flex max-w-screen-2xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
       >
         <div className="flex items-center gap-x-12">
