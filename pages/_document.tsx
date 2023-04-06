a7fbe1f28517966d216dd9a96d302dad5cfac2ca
James Singleton <jamesrsingleton1@gmail.com>
2023-04-06 11:06:17 -0700

    chore: remove font loading in _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        <Head>
-          <link rel="preload" href="/fonts/CalSans-SemiBold.woff2" as="font" type="font/woff2" />
-        </Head>
+        <Head />
         <body className="loading bg-white text-slate-600 antialiased">
           <Main />
           <NextScript />
