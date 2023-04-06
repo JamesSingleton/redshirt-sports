63b3836bcba89e1c2978307f0cb4cee8aea976d8
James Singleton <jamesrsingleton1@gmail.com>
2023-04-05 21:16:09 -0700

    chore(deps): update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -79,7 +79,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('@tailwindcss/typography'),
   ],
 }
