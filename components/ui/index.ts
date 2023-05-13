66e0d5b9510ff8e9dd0358dfda9940690381bcd8
James Singleton <jamesrsingleton1@gmail.com>
2023-05-13 11:02:15 -0700

    chore: remove imports of deleted files

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,12 +1,10 @@
 import Container from './Container'
 import Pagination from './Pagination'
 import Date from './Date'
-import BlurImage from './BlurImage'
 import PageHeader from './PageHeader'
 import HorizontalCard from './HorizontalCard'
 import VerticalArticleCard from './VerticalArticleCard'
 import MinimalHorizontalCard from './MinimalHorizontalCard'
-import Tweet from './Tweet'
 import Breadcrumbs from './Breadcrumbs'
 import CategoryHeader from './CategoryHeader'
 
@@ -14,12 +12,10 @@ export {
   Container,
   Pagination,
   Date,
-  BlurImage,
   PageHeader,
   HorizontalCard,
   VerticalArticleCard,
   MinimalHorizontalCard,
-  Tweet,
   Breadcrumbs,
   CategoryHeader,
 }
