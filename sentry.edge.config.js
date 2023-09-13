72c1710ccc44f5a43aa875bbde7c9698d8d889c2
James Singleton <jamesrsingleton1@gmail.com>
2023-09-12 19:37:58 -0700

    chore: adjust tracesSampleRate

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -6,7 +6,7 @@ Sentry.init({
   dsn: process.env.NODE_ENV === 'production' ? SENTRY_DSN : null,
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.5,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
