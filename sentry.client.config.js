83dbd6c88549b5005c286eef94e3e7011dfc5f44
James Singleton <jamesrsingleton1@gmail.com>
2023-09-27 08:07:00 -0700

    chore: add ignoreErrors to sentry

--- sentry.client.config.js
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/nextjs'
-
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-
-Sentry.init({
-  dsn: process.env.NODE_ENV === 'production' ? SENTRY_DSN : null,
-  tracesSampleRate: 0.5,
-})
