9719b264858525877a0fed9d67e59b75065358ef
James Singleton <jamesrsingleton1@gmail.com>
2023-10-20 16:34:31 -0700

    fix: incorrect dates showing

--- components/ui/Date.tsx
+++ components/ui/Date.tsx
@@ -1,4 +1,5 @@
 import { parseISO, format } from 'date-fns'
+import { utcToZonedTime } from 'date-fns-tz'
 
 import type { WithClassName } from '@types'
 
@@ -6,12 +7,14 @@ interface DateProps extends WithClassName {
   dateString: string
 }
 
+const timeZone = 'America/Phoenix'
+
 export default function Date({ dateString, className }: DateProps) {
   const date = parseISO(dateString)
 
   return (
     <time dateTime={dateString} className={className} suppressHydrationWarning>
-      {format(date, 'LLL d, yyyy')}
+      {format(utcToZonedTime(date, timeZone), 'LLL d, yyyy')}
     </time>
   )
 }
