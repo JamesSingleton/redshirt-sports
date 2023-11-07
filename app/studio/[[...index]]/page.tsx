c7d4c85eacdbe0296eaa51e0b2c0c580c783dd51
James Singleton <jamesrsingleton1@gmail.com>
2023-11-07 12:54:42 -0700

    chore: export sanity.io viewport

--- app/studio/[[...index]]/page.tsx
+++ app/studio/[[...index]]/page.tsx
@@ -3,6 +3,7 @@ import Studio from './Studio'
 export const dynamic = 'force-static'
 
 export { metadata } from 'next-sanity/studio/metadata'
+export { viewport } from 'next-sanity/studio/viewport'
 
 export default function StudioPage() {
   return <Studio />
