e2151d3768a0cc54aa6744aeb55186c683b53921
James Singleton <jamesrsingleton1@gmail.com>
2023-11-30 08:56:50 -0700

    chore: update deps and sentry traceSampleRate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,7 +4,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: process.env.NODE_ENV === 'production' ? SENTRY_DSN : undefined,
-  tracesSampleRate: 0.5,
+  tracesSampleRate: 0.25,
   ignoreErrors: [
     /AboutError/,
     'ResizeObserver loop limit exceeded',
