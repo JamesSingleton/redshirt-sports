8cb93a65e4b7aab0234be6444dfeabe809404268
James Singleton <jamesrsingleton1@gmail.com>
2024-05-18 18:52:14 -0700

    chore: different OG image for articles

--- app/(website)/[slug]/opengraph-image.tsx
+++ app/(website)/[slug]/opengraph-image.tsx
@@ -13,43 +13,46 @@ export default async function ArticleOGImage({ params: { slug } }: { params: { s
 
   return new ImageResponse(
     (
-      <div tw="relative flex flex-col bg-[#DC2727] w-[1200px] h-[630px] px-8 text-white justify-center">
-        <h1 tw="font-bold text-6xl leading-tight mt-8 max-w-[50%]">{article.title}</h1>
-        <div tw="flex items-center mt-8">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            alt="Author's avatar"
-            tw="w-12 h-12 rounded-full border-2 border-white"
-            height="48"
-            src={urlForImage(article.author.image).width(48).height(48).url()}
-            style={{
-              objectFit: 'cover',
-            }}
-            width="48"
-          />
-          <div tw="ml-4 flex flex-col">
-            <span tw="font-semibold">{article.author.name}</span>
-            <span tw="text-sm">
-              {new Date(article.publishedAt).toLocaleDateString('en-US', {
-                month: 'short',
-                day: 'numeric',
-                year: 'numeric',
-              })}
-            </span>
+      <div key="1" tw="relative bg-white w-[1200px] h-[630px] flex">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          alt={article.mainImage.caption}
+          tw="absolute top-0 left-0 w-full h-full"
+          height="630"
+          src={urlForImage(article.mainImage).width(1200).height(630).url()}
+          style={{
+            objectFit: 'cover',
+          }}
+          width="1200"
+        />
+        <span tw="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-30" />
+        <div tw="absolute bottom-0 left-0 p-8 flex">
+          <div tw="bg-white rounded-lg p-6 max-w-md flex flex-col">
+            <h1 tw="text-4xl font-bold leading-tight">{article.title}</h1>
+            <div tw="flex items-center mt-4">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                alt={article.author.name}
+                tw="h-12 w-12 rounded-full"
+                height="48"
+                src={urlForImage(article.author.image).width(48).height(48).url()}
+                style={{
+                  objectFit: 'cover',
+                }}
+                width="48"
+              />
+              <div tw="ml-3 flex flex-col">
+                <span tw="font-semibold">{article.author.name}</span>
+                <span tw="text-sm">
+                  {new Date(article.publishedAt).toLocaleDateString('en-US', {
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric',
+                  })}
+                </span>
+              </div>
+            </div>
           </div>
-        </div>
-        <div tw="absolute right-0 w-[50%] h-full flex justify-end">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            alt={article.mainImage.caption}
-            tw="rounded-l-full h-full"
-            height="630"
-            src={urlForImage(article.mainImage).width(600).height(630).url()}
-            style={{
-              objectFit: 'cover',
-            }}
-            width="600"
-          />
         </div>
       </div>
     ),
