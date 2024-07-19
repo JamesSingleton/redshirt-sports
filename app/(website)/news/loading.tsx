dc9e9e697de76ad2783705d953b2b8d3f5b01442
James Singleton <jamesrsingleton1@gmail.com>
2024-07-19 07:14:36 -0700

    chore: loading screen for articles

--- app/(website)/news/loading.tsx
+++ app/(website)/news/loading.tsx
@@ -1,4 +1,5 @@
 import { Skeleton } from '@/components/ui/skeleton'
+import { LoadingArticle } from './_components/loading-article'
 
 export default function Loading() {
   return (
@@ -14,27 +15,10 @@ export default function Loading() {
       </section>
       <section className="container pb-12 sm:pb-16 lg:pb-20 xl:pb-24">
         <div className="mt-8 grid grid-cols-1 gap-12 sm:grid-cols-2 lg:mt-12 lg:grid-cols-3 xl:gap-16">
-          <div className="flex flex-col space-y-3">
-            <Skeleton className="h-[205px] w-[411px] rounded-xl" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex flex-col space-y-3">
-            <Skeleton className="h-[205px] w-[411px] rounded-xl" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
-          <div className="flex flex-col space-y-3">
-            <Skeleton className="h-[205px] w-[411px] rounded-xl" />
-            <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px]" />
-              <Skeleton className="h-4 w-[200px]" />
-            </div>
-          </div>
+          <LoadingArticle />
+          <LoadingArticle />
+          <LoadingArticle />
+          <LoadingArticle />
         </div>
       </section>
     </>
