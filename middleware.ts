1ae55f39a45d304d2e85bd6c95e716cc1945e5d8
James Singleton <jamesrsingleton1@gmail.com>
2024-07-23 12:58:53 -0700

    chore: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,36 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
+type UserMetadata = {
+  isVoter?: boolean
+}
+
 const isOnboardingRoute = createRouteMatcher(['/vote/onboarding'])
 const isProtectedRoute = createRouteMatcher(['/admin(.*)', '/vote(.*)'])
 
 export default clerkMiddleware((auth, req: NextRequest) => {
-  if (isProtectedRoute(req)) auth().protect()
   const { userId, sessionClaims, redirectToSignIn } = auth()
+  if (isProtectedRoute(req)) {
+    auth().protect()
+    const { isVoter } = sessionClaims?.metadata as UserMetadata
+    if (!isVoter) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+  }
 
   if (userId && isOnboardingRoute(req)) {
     return NextResponse.next()
   }
 
-  // If the user isn't signed in and the route is protected, redirect to sign in
   if (!userId && isProtectedRoute(req)) {
     return redirectToSignIn({ returnBackUrl: req.url })
   }
 
-  // Catch users who do not have `onboardingComplete: true` in their publicMetadata
-  // Redirect them to the /onboading route to complete onboarding
   if (userId && !sessionClaims?.metadata?.onboardingComplete) {
     const onboardingUrl = new URL('/vote/onboarding', req.url)
     return NextResponse.redirect(onboardingUrl)
   }
 
-  // If the user is logged in and the route is protected, let them view.
   if (userId && isProtectedRoute(req)) {
     return NextResponse.next()
   }
