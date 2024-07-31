6ba49cdab38051b91f387d092010df5a0bfd21e9
James Singleton <jamesrsingleton1@gmail.com>
2024-07-30 20:27:39 -0700

    chore: some minor changes from livestream

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,5 +1,3 @@
-import '../globals.css'
-
 import PlausibleProvider from 'next-plausible'
 
 import { SiteHeader, Footer, ThemeProvider } from '@components/common'
--- sanity/plugins/deskStructure.jsx
+++ sanity/plugins/deskStructure.jsx
@@ -41,8 +41,6 @@ export const deskStructure = async (S, context) => {
         }
         return 0
       }),
-    // S.documentTypeListItem('player').title('Player Profiles'),
-    // S.documentTypeListItem('transferPortal').title('Transfer Portal Players'),
     S.listItem()
       .title('Schools')
       .icon(School)
