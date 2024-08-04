9cde92f0d257dd22ed614c74b1dd46b475bea18d
James Singleton <jamesrsingleton1@gmail.com>
2024-08-04 14:00:11 -0700

    fix: undo middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -42,7 +42,11 @@ export default clerkMiddleware((auth, req: NextRequest) => {
   }
 })
 
-// run middleware on all routes that start with /admin, /vote, or /onboarding
 export const config = {
-  matcher: ['/admin/:path*', '/vote/:path*', '/onboarding/:path*'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
