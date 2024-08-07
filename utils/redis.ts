528f7e3c7241853c9f1902463e689e19e70c49e1
James Singleton <jamesrsingleton1@gmail.com>
2024-08-06 21:40:59 -0700

    chore: use Redis.fromEnv()

--- utils/redis.ts
+++ utils/redis.ts
@@ -4,9 +4,6 @@ if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
   throw new Error('UPSTASH_REDIS_REST_TOKEN is not set')
 }
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+const redis = Redis.fromEnv()
 
 export default redis
