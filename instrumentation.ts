50c5a60ee77a7c4235d9083572b0e1c306a9d752
James Singleton <jamesrsingleton1@gmail.com>
2024-08-07 09:22:41 -0700

    chore: update Sentry integration

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,27 +1,9 @@
-import * as Sentry from '@sentry/nextjs'
-
-export function register() {
+export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    Sentry.init({
-      dsn: 'https://0345d0cacda34590b6139082a4521911@o1192929.ingest.us.sentry.io/6314703',
-      tracesSampleRate: 0.25,
-      ignoreErrors: [
-        /AboutError/,
-        'ResizeObserver loop limit exceeded',
-        'ResizeObserver loop completed with undelivered notifications.',
-      ],
-    })
+    await import('./sentry.server.config')
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
-    Sentry.init({
-      dsn: 'https://0345d0cacda34590b6139082a4521911@o1192929.ingest.us.sentry.io/6314703',
-      tracesSampleRate: 0.25,
-      ignoreErrors: [
-        /AboutError/,
-        'ResizeObserver loop limit exceeded',
-        'ResizeObserver loop completed with undelivered notifications.',
-      ],
-    })
+    await import('./sentry.edge.config')
   }
 }
