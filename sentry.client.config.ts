dd85ab5c39714723aac3c55a5c493c576c608769
James Singleton <jamesrsingleton1@gmail.com>
2024-08-07 21:54:22 -0700

    chore: update sentry.client.config.ts to be 0 for session replay

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -16,14 +16,14 @@ Sentry.init({
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
     Sentry.replayIntegration({
       // Additional Replay configuration goes in here, for example:
-      maskAllText: true,
-      blockAllMedia: true,
+      maskAllText: false,
+      blockAllMedia: false,
     }),
   ],
 })
