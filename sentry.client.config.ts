bb8550a5e9abe5e460de4f827918c8eeaa94c14b
James Singleton <jamesrsingleton1@gmail.com>
2024-08-08 09:07:49 -0700

    chore: replaySessionSampleRate to 0.25

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -16,7 +16,7 @@ Sentry.init({
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0,
+  replaysSessionSampleRate: 0.25,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
