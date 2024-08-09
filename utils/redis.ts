e525243f0faafb5b972d39a3d6d93cf6ecdb78f1
James Singleton <jamesrsingleton1@gmail.com>
2024-08-09 08:25:04 -0700

    chore: add some logs

--- utils/redis.ts
+++ utils/redis.ts
@@ -1,9 +1,13 @@
 import { Redis } from '@upstash/redis'
 
-if (!process.env.UPSTASH_REDIS_REST_TOKEN) {
-  throw new Error('UPSTASH_REDIS_REST_TOKEN is not set')
+if (!process.env.UPSTASH_REDIS_REST_TOKEN && !process.env.UPSTASH_REDIS_REST_URL) {
+  throw new Error('UPSTASH_REDIS_REST_TOKEN and or UPSTASH_REDIS_REST_URL is not set')
 }
 
+console.log('Before Redis.fromEnv()')
+
 const redis = Redis.fromEnv()
+
+console.log('After Redis.fromEnv()', redis)
 
 export default redis
