05238886a3efe71de9d6babc87ac30a7aa2eb8bf
James Singleton <jamesrsingleton1@gmail.com>
2024-08-09 18:12:11 -0700

    chore: some cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,7 @@ import type { Config } from 'tailwindcss'
 
 const config = {
   darkMode: ['class'],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
