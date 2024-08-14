ea2fb1f3cd91ee36aa9a1ac67738a53b4409ea29
James Singleton <jamesrsingleton1@gmail.com>
2024-08-13 17:04:00 -0700

    chore: up the maxValueLength for sentry errors

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,15 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://0345d0cacda34590b6139082a4521911@o1192929.ingest.us.sentry.io/6314703",
+  dsn: 'https://0345d0cacda34590b6139082a4521911@o1192929.ingest.us.sentry.io/6314703',
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
+  maxValueLength: 500,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+})
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -12,6 +12,7 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  maxValueLength: 500,
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
