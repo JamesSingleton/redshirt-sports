dfc9aaefb0562174c9ac969cbf2d8117eca61ce4
James Singleton <jamesrsingleton1@gmail.com>
2024-08-13 22:13:26 -0700

    chore: update tailwindcss to 3.4.10 and comment out static build returning empty array (#89)

--- app/(website)/[slug]/page.tsx
+++ app/(website)/[slug]/page.tsx
@@ -70,9 +70,9 @@ export async function generateMetadata({ params }: PageProps): Promise<Metadata>
 export async function generateStaticParams() {
   const postPaths = await getPostsPaths()
 
-  if (process.env.VERCEL_ENV === 'preview') {
-    return []
-  }
+  // if (process.env.VERCEL_ENV === 'preview') {
+  //   return []
+  // }
 
   return postPaths.map((path) => ({
     slug: path,
