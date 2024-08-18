403fa25d7fa43a6b60f3e3128e88f5fd4826cb58
James Singleton <jamesrsingleton1@gmail.com>
2024-08-17 17:32:16 -0700

    chore(sentry): remove replays

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,8 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
   dsn: 'https://0345d0cacda34590b6139082a4521911@o1192929.ingest.us.sentry.io/6314703',
-
-  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
   maxValueLength: 500,
-
-  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
 })
