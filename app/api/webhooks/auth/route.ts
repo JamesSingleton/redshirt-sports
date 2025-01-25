f3471197c322d5ce07ab60cc399e774a91163dbe
James Singleton <jamesrsingleton1@gmail.com>
2025-01-25 10:45:36 -0700

    chore: update headers

--- app/api/webhooks/auth/route.ts
+++ app/api/webhooks/auth/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
-  const headerPayload = headers()
+  const headerPayload = await headers()
   const svix_id = headerPayload.get('svix-id')
   const svix_timestamp = headerPayload.get('svix-timestamp')
   const svix_signature = headerPayload.get('svix-signature')
