7ac334ae844f821c447ad474d45808193b7a46a1
James Singleton <jamesrsingleton1@gmail.com>
2025-02-27 16:54:35 -0700

    chore: update env.ts

--- app/env.ts
+++ app/env.ts
@@ -1,9 +1,7 @@
 import { createEnv } from '@t3-oss/env-nextjs'
-import { vercel } from '@t3-oss/env-nextjs/presets'
 import { z } from 'zod'
 
 export const env = createEnv({
-  extends: [vercel()],
   shared: {
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
   },
