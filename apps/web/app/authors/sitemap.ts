e42d634a29e61728ac59b8e9847e8d17c3d121ea
James Singleton <jamesrsingleton1@gmail.com>
2025-06-17 17:34:40 -0700

    fix: do not use sanityFetch in sitemap

--- apps/web/app/authors/sitemap.ts
+++ apps/web/app/authors/sitemap.ts
@@ -1,23 +1,18 @@
 import { HOME_DOMAIN } from '@/lib/constants'
-import { sanityFetch } from '@/lib/sanity/live'
+import { client } from '@/lib/sanity/client'
 
 import type { MetadataRoute } from 'next'
 import type { Author } from '@/lib/sanity/sanity.types'
 
 export const dynamic = 'force-dynamic'
 
-async function fetchAuthorsSitemapData() {
-  return await sanityFetch({
-    query: `*[_type == 'author' && defined(slug.current) && archived == false]{
+export async function generateSitemaps() {
+  const authors =
+    await client.fetch(`*[_type == 'author' && defined(slug.current) && archived == false]{
       _id,
       _updatedAt,
       "slug": slug.current
-    }`,
-  })
-}
-
-export async function generateSitemaps() {
-  const { data: authors } = await fetchAuthorsSitemapData()
+    }`)
 
   const sitemaps = []
   for (let i = 0; i < Math.ceil(authors.length / 50000); i++) {
@@ -30,7 +25,12 @@ export async function generateSitemaps() {
 }
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { data: authors } = await fetchAuthorsSitemapData()
+  const authors =
+    await client.fetch(`*[_type == 'author' && defined(slug.current) && archived == false]{
+      _id,
+      _updatedAt,
+      "slug": slug.current
+    }`)
 
   return authors.map((author: Author) => ({
     url: `${HOME_DOMAIN}/authors/${author.slug}`,
