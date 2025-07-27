478606f6ed78b67493c908949dc9a0f6e2e5f036
James Singleton <jamesrsingleton1@gmail.com>
2025-07-27 09:48:34 -0700

    chore: update sitemap for rankings

--- apps/web/app/college/[sport]/rankings/sitemap.ts
+++ apps/web/app/college/[sport]/rankings/sitemap.ts
@@ -14,7 +14,7 @@ const baseUrl = getBaseUrl()
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const yearsWithVotes = await getYearsWithVotes()
   return yearsWithVotes.map(({ year, week, division }) => ({
-    url: `${baseUrl}/college/football/rankings/${division}/${year}/${week}`,
+    url: `${baseUrl}/college/football/rankings/${division}/${year}/${week === 999 ? 'final-rankings' : week}`,
     lastModified: new Date(),
     priority: 0.7,
   }))
