67317ae7cd4a242d8facb6846cd8feefac29d0ed
James Singleton <jamesrsingleton1@gmail.com>
2025-09-13 11:35:09 -0700

    feat: use style new-york instead of default

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    reactCompiler: true,
+    inlineCss: true,
+  },
 }
 
 export default nextConfig
