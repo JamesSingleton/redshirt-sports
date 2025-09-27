d81aba24adbe082312d4491b27181bc2282827f6
James Singleton <jamesrsingleton1@gmail.com>
2025-09-26 20:01:55 -0700

    feat(studio): add blueprints/functions

---
+++ apps/studio/sanity.blueprint.ts
@@ -0,0 +1,30 @@
+import { defineBlueprint, defineDocumentFunction } from '@sanity/blueprints'
+
+export default defineBlueprint({
+  resources: [
+    defineDocumentFunction({
+      type: 'sanity.function.document',
+      name: 'published-at',
+      src: './functions/published-at',
+      memory: 1,
+      timeout: 10,
+      event: {
+        on: ['publish'],
+        filter: "_type == 'post' && !defined(publishedAt)",
+        projection: '{_id}',
+      },
+    }),
+    defineDocumentFunction({
+      type: 'sanity.function.document',
+      name: 'auto-redirect',
+      src: './functions/auto-redirect',
+      memory: 2,
+      timeout: 30,
+      event: {
+        on: ['publish'],
+        filter: "_type == 'post' && delta::changedAny(slug.current)",
+        projection: "{'beforeSlug': before().slug.current, 'slug': after().slug.current}",
+      },
+    }),
+  ],
+})
