a2adae33ffdcff91904d108b8eb2270b64f5467e
James Singleton <jamesrsingleton1@gmail.com>
2025-11-09 11:21:02 -0700

    fix(sitemaps): fix the lastModified dates

--- apps/web/app/college/sitemap.ts
+++ apps/web/app/college/sitemap.ts
@@ -7,10 +7,9 @@ export const dynamic = 'force-dynamic'
 
 const baseUrl = getBaseUrl()
 
-const POSTS_QUERY = `*[_type == "post" && defined(sport->slug.current)]{
+const POSTS_QUERY = `*[_type == "post" && defined(sport->slug.current)]| order(publishedAt desc){
   "sport": sport->slug.current,
   "division": division->slug.current,
-  "divisionTitle": division->title,
   "sportSubgrouping": sportSubgrouping->slug.current,
   "conferences": conferences[]->slug.current,
   _updatedAt
@@ -18,13 +17,16 @@ const POSTS_QUERY = `*[_type == "post" && defined(sport->slug.current)]{
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await client.fetch(POSTS_QUERY)
-  const urls = new Map()
+  const urls = new Map<string, string>()
 
   posts.forEach((post) => {
     if (post.sport) {
       // /college/[sport]/news
       const url1 = `${baseUrl}/college/${post.sport}/news`
-      urls.set(url1, post._updatedAt)
+      const currentTimestamp1 = urls.get(url1)
+      if (!currentTimestamp1 || new Date(post._updatedAt) > new Date(currentTimestamp1)) {
+        urls.set(url1, post._updatedAt)
+      }
 
       // Check for Division I/D1 and require sportSubgrouping
       const isD1 =
@@ -44,13 +46,19 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       if (divisionSegment) {
         // /college/[sport]/news/[division or sportSubgrouping]
         const url2 = `${baseUrl}/college/${post.sport}/news/${divisionSegment}`
-        urls.set(url2, post._updatedAt)
+        const currentTimestamp2 = urls.get(url2)
+        if (!currentTimestamp2 || new Date(post._updatedAt) > new Date(currentTimestamp2)) {
+          urls.set(url2, post._updatedAt)
+        }
         if (post.conferences && post.conferences.length > 0) {
           post.conferences.forEach((conference) => {
             if (conference) {
               // /college/[sport]/news/[division or sportSubgrouping]/[conference]
               const url3 = `${baseUrl}/college/${post.sport}/news/${divisionSegment}/${conference}`
-              urls.set(url3, post._updatedAt)
+              const currentTimestamp3 = urls.get(url3)
+              if (!currentTimestamp3 || new Date(post._updatedAt) > new Date(currentTimestamp3)) {
+                urls.set(url3, post._updatedAt)
+              }
             }
           })
         }
