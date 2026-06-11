792185f1365d72c1ff44e9b9498b50462d7cc5dc
James Singleton <jamesrsingleton1@gmail.com>
2026-06-10 22:22:40 -0700

    fix(studio): hardcode url to post to

--- apps/studio/functions/cache-invalidate/index.ts
+++ apps/studio/functions/cache-invalidate/index.ts
@@ -1,19 +1,20 @@
 import { syncTagInvalidateEventHandler } from "@sanity/functions";
 
-const REVALIDATE_URL = `${process.env.SANITY_STUDIO_PRESENTATION_URL}/api/revalidate-tags`;
-
 export const handler = syncTagInvalidateEventHandler(
   async ({ event, done }) => {
     const { syncTags } = event.data;
 
-    const res = await fetch(REVALIDATE_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        // Authorization: `Bearer ${process.env.SANITY_REVALIDATE_TAGS_SECRET}`,
+    const res = await fetch(
+      "https://www.redshirtsports.xyz/api/revalidate-tags",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${process.env.SANITY_REVALIDATE_TAGS_SECRET}`,
+        },
+        body: JSON.stringify({ tags: syncTags }),
       },
-      body: JSON.stringify({ tags: syncTags }),
-    });
+    );
     console.log(`Revalidated ${syncTags.length} tags, HTTP ${res.status}`);
 
     const response = await done(syncTags);
